695220c9d0945b23115e37f8feb553ab7f94bc6b
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-06-25 18:26:34 +0300

    add branch drawing function

--- src/script.js
+++ src/script.js
@@ -3,27 +3,52 @@ window.addEventListener("load", function () {
   const canvas = this.document.getElementById("fractalsCanvas");
   /** @type {CanvasRenderingContext2D} */
   const ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth * 0.8;
-  canvas.height = window.innerHeight * 0.8;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
   // canvas styles
   ctx.fillStyle = "green";
   ctx.strokeStyle = "blue";
-  ctx.lineWidth = 10;
+  ctx.lineWidth = 25;
   ctx.lineCap = "round";
 
   // effect settings
-  let size = 600;
-  let sides = (Math.random() * 40 + 1).toFixed(0);
+  let size = 400;
+  let maxDepth = 5;
+  let directionOfBranch = 0.8;
+  let shrink = 0.5;
+  let branches = 2;
+
   ctx.save();
   ctx.translate(canvas.width / 2, canvas.height / 2);
-  ctx.scale(0.5, 0.5);
+  ctx.scale(1, 1);
+  ctx.rotate(0);
 
-  for (let i = 0; i < sides; i++) {
+  function drawBranch(depth) {
+    if (depth > maxDepth) {
+      return;
+    }
     ctx.beginPath();
     ctx.moveTo(0, 0);
-    ctx.lineTo(size, canvas.height / 2);
+    ctx.lineTo(size, 0);
     ctx.stroke();
-    ctx.rotate((Math.PI * 2) / sides);
+
+    for (let i = 0; i < branches; i++) {
+      ctx.save();
+      ctx.translate(size - (size / branches) * i, 0);
+      ctx.rotate(directionOfBranch);
+      ctx.scale(shrink, shrink);
+      drawBranch(depth + 1);
+      ctx.restore();
+
+      ctx.save();
+      ctx.translate(size - (size / branches) * i, 0);
+      ctx.rotate(-directionOfBranch);
+      ctx.scale(shrink, shrink);
+      drawBranch(depth + 1);
+      ctx.restore();
+    }
   }
+
+  drawBranch(0);
 });
